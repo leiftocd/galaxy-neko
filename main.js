9e715792e18ee589782dbd5967fce31153f9e9d6
wongwongkarma <wongwong@karma.today>
2025-08-01 11:47:45 +0700

    update mb

--- main.js
+++ main.js
@@ -1,11 +1,32 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const rocket = document.querySelector(".rocket");
+function copyCode(button) {
+    const container = button.closest(".ca-code");
+    const text = container.querySelector(".code-text").textContent;
+    const tooltip = button.querySelector(".tooltip");
 
-    rocket.addEventListener("mouseenter", () => {
-        rocket.classList.add("fly");
-
+    navigator.clipboard.writeText(text).then(() => {
+        tooltip.classList.add("show");
         setTimeout(() => {
-            rocket.classList.remove("fly");
-        }, 2000);
+            tooltip.classList.remove("show");
+        }, 1500);
     });
+}
+const menuBtn = document.getElementById("menu-toggle");
+const submenu = document.getElementById("submenu");
+const closeBtn = document.getElementById("menu-close");
+
+// Toggle submenu
+menuBtn.addEventListener("click", () => {
+    submenu.classList.add("active");
+});
+
+// Close when clicking close icon
+closeBtn.addEventListener("click", () => {
+    submenu.classList.remove("active");
+});
+
+// Close when clicking outside submenu content
+submenu.addEventListener("click", (e) => {
+    if (e.target === submenu) {
+        submenu.classList.remove("active");
+    }
 });
